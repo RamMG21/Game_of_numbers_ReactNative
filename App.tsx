38a7d8a7628ee866b4b34f2e19f63fe258e6f2f7
Ramses-Moreno <ram.ramses21sentinela@gmail.com>
2023-11-19 10:23:15 -0800

    background gradient
    Last step to start with the game logic programation.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
+import LinearGradient from 'react-native-linear-gradient';
 
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <StartGameScreen/>
+    <LinearGradient colors={['#44000B', '#E0455F']} style={styles.rootScreen}>
+        
+       <StartGameScreen/>
+   
+    </LinearGradient>
+    
+   
   );
 }
 
@@ -16,4 +23,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  rootScreen: {
+    flex: 1,
+   
+  },
+  
 });
